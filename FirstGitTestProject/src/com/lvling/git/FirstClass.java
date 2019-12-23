caf04c86ed666a72760f6140531b9a1c1558dae0
LIFE.FE <zhoufei9966@live.com>
2019-12-23 23:52:12 +0800

    ideaiu添加到git

---
+++ FirstGitTestProject/src/com/lvling/git/FirstClass.java
@@ -0,0 +1,16 @@
+package com.lvling.git;
+
+/**
+ * @Author: LIFE.FE
+ * @Date: 2019/12/23 23:33
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
