5b7d4d13bc16464263f14fa20d08d7ca31cc65e3
LIFE.FE <zhoufei9966@live.com>
2019-12-24 00:00:57 +0800

    ideaiu添加内容

--- FirstGitTestProject/src/com/lvling/git/FirstClass.java
+++ FirstGitTestProject/src/com/lvling/git/FirstClass.java
@@ -16,7 +16,7 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
 
-
+        System.out.println("hello git project");
     }
 
 
